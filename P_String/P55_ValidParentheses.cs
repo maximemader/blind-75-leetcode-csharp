3774159f7bf0c3b461faa573402e6c202d6fa27f
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-15 15:52:50 +0100

    P55 & P56

--- P_String/P55_ValidParentheses.cs
+++ P_String/P55_ValidParentheses.cs
@@ -1,6 +1,30 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Valid Parentheses
+/// https://leetcode.com/problems/valid-parentheses/
+/// </summary>
 public class P55_ValidParentheses
 {
-    
+    public bool IsValid(string s) 
+    {
+        var stack = new Stack<char>();
+        
+        foreach (var c in s)
+        {
+            if (c is '(' or '[' or '{')
+                stack.Push(c);
+            else if (stack.Count == 0)
+                return false;
+            else switch (c)
+            {
+                case ')' when stack.Pop() != '(':
+                case ']' when stack.Pop() != '[':
+                case '}' when stack.Pop() != '{':
+                    return false;
+            }
+        }
+        
+        return stack.Count == 0;
+    }
 }
--- P_String/P56_ValidPalindrome.cs
+++ P_String/P56_ValidPalindrome.cs
@@ -1,6 +1,56 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Valid Palindrome
+/// https://leetcode.com/problems/valid-palindrome/
+/// </summary>
 public class P56_ValidPalindrome
 {
-    
+    public bool IsPalindrome(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            // If bored we could benchmark a custom implementation since the input
+            // is ASCII only.
+            if (!char.IsLetterOrDigit(s[left]))
+            {
+                ++left;
+            }
+            else if (!char.IsLetterOrDigit(s[right]))
+            {
+                --right;
+            }
+            else if (char.ToLower(s[left]) != char.ToLower(s[right]))
+            {
+                return false;
+            }
+            else
+            {
+                ++left;
+                --right;
+            }
+        }
+
+        return true;
+    }
+
+    // Easy mode ;)
+    // public bool IsPalindrome(string s) 
+    // {
+    //     var sanitized = s
+    //         .Where(char.IsLetterOrDigit)
+    //         .Select(char.ToLower)
+    //         .ToArray();
+    //     
+    //     for(var i = 0; i < sanitized.Length / 2; ++i)
+    //     {
+    //         if (sanitized[i] != sanitized[sanitized.Length - 1 - i])
+    //             return false;
+    //     }
+    //
+    //     return true;
+    // }
 }
