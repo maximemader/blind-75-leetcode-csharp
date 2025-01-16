643dc8011e1379f9ee4966d12f0f347d39689ab0
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-16 15:04:05 +0100

    P50

--- P_String/P50_LongestSubstringWithoutRepeatingCharacters.cs
+++ P_String/P50_LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,6 +1,36 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Longest Substring Without Repeating Characters
+/// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+/// </summary>
 public class P50_LongestSubstringWithoutRepeatingCharacters
 {
-    
+    public int LengthOfLongestSubstring(string s)
+    {
+        if(string.IsNullOrEmpty(s))
+            return 0;
+
+        var chars = new HashSet<char>();
+
+        var left = 0;
+        var right = 0;
+        var maxLength = 0;
+        
+        while (right < s.Length)
+        {
+            if(!chars.Add(s[right]))
+            {
+                chars.Remove(s[left]);
+                ++left;
+            }
+            else
+            {
+                ++right;
+                maxLength = Math.Max(maxLength, right - left);
+            }
+        }
+
+        return maxLength;
+    }
 }
