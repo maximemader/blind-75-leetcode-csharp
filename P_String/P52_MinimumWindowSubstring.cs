6e35103de490f2e9c530adf5ca780cf4fe3cc499
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-16 12:54:15 +0100

    P52 & P54

--- P_String/P52_MinimumWindowSubstring.cs
+++ P_String/P52_MinimumWindowSubstring.cs
@@ -1,6 +1,74 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Minimum Window Substring
+/// https://leetcode.com/problems/minimum-window-substring/
+/// </summary>
 public class P52_MinimumWindowSubstring
 {
-    
+    public string MinWindow(string s, string t) 
+    {
+        if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t) || s.Length < t.Length)
+            return string.Empty;
+        
+        var frequencyMap = new Dictionary<char, int>();
+
+        foreach (var c in t)
+        {
+            if(!frequencyMap.TryAdd(c, 1))
+                ++frequencyMap[c];
+        }
+
+        // We can't go map.Values.All() because it's too slow. So instead, we keep track
+        // of the number of unique characters in t that don't match the right frequency.
+        var count = frequencyMap.Count;
+
+        var left = 0;
+        var right = 0;
+
+        var minWindowSize = int.MaxValue;
+        var window = (Start: 0, Size: 0);
+
+        while (right < s.Length)
+        {
+            if(frequencyMap.ContainsKey(s[right]))
+            {
+                --frequencyMap[s[right]];
+                
+                if(frequencyMap[s[right]] == 0)
+                    --count;
+            }
+            
+            if(count == 0)
+            {
+                var windowSize = right-left+1;
+
+                if (windowSize < minWindowSize)
+                {
+                    minWindowSize = windowSize;
+                    window = (left, right - left + 1);
+                }
+                
+                while(!frequencyMap.ContainsKey(s[left]) || 
+                      (frequencyMap.ContainsKey(s[left]) && frequencyMap[s[left]] < 0)) 
+                {
+                    if(frequencyMap.ContainsKey(s[left]) && frequencyMap[s[left]] < 0) 
+                        ++frequencyMap[s[left]];
+                    
+                    ++left;
+                    windowSize = right - left + 1;
+
+                    if(windowSize < minWindowSize) 
+                    {
+                        window = (left, right - left + 1);
+                        minWindowSize = windowSize;
+                    }
+                }
+            }
+
+            ++right;
+        }
+
+        return minWindowSize == int.MaxValue ? string.Empty : s.Substring(window.Start, window.Size);
+    }
 }
--- P_String/P54_GroupAnagrams.cs
+++ P_String/P54_GroupAnagrams.cs
@@ -1,6 +1,37 @@
+// ReSharper disable ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Group Anagrams
+/// https://leetcode.com/problems/group-anagrams/
+/// </summary>
 public class P54_GroupAnagrams
 {
-    
+    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    {
+        if (strs == null || strs.Length == 0) {
+            return new List<IList<string>>();
+        }
+        
+        var map = new Dictionary<string, IList<string>>();
+        
+        // Since we have only ASCII lowercase letters, we can go a bit faster
+        // by sorting characters instead of reusing P53_ValidAnagram.cs.
+        foreach(var str in strs)
+        {
+            // Same thing, but slower.
+            //var key = new string(str.OrderBy(c => c).ToArray());
+            
+            var strArray = str.ToCharArray();
+            Array.Sort(strArray);
+            var key = new string(strArray);
+            
+            if(!map.ContainsKey(key))
+                map[key] = [];
+            
+            map[key].Add(str);
+        }
+
+        return map.Values.ToList();
+    }
 }
