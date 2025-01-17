d97de7810c3b4e1448809c518e3eb986fdb08240
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-17 12:23:54 +0100

    P47 and P48

--- P_Matrix/P47_SpiralMatrix.cs
+++ P_Matrix/P47_SpiralMatrix.cs
@@ -1,6 +1,49 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Spiral Matrix
+/// https://leetcode.com/problems/spiral-matrix/
+/// </summary>
 public class P47_SpiralMatrix
 {
-    
+    public IList<int> SpiralOrder(int[][] matrix) {
+        if(matrix.Length == 0 || matrix[0].Length == 0)
+            return new List<int>();
+        
+        var result = new List<int>();
+        
+        var top = 0;
+        var bottom = matrix.Length - 1;
+        var left = 0;
+        var right = matrix[0].Length - 1;
+
+        while (true)
+        {
+            for(int i = left; i <= right; ++i)
+                result.Add(matrix[top][i]);
+
+            if(++top > bottom)
+                break;
+            
+            for(int i = top; i <= bottom; ++i)
+                result.Add(matrix[i][right]);
+
+            if(left > --right)
+                break;
+            
+            for(int i = right; i >= left; --i)
+                result.Add(matrix[bottom][i]);
+
+            if(top > --bottom)
+                break;
+            
+            for(int i = bottom; i >= top; --i)
+                result.Add(matrix[i][left]);
+            
+            if(++left > right)
+                break;
+        }
+
+        return result;
+    }
 }
--- P_Matrix/P48_RotateImage.cs
+++ P_Matrix/P48_RotateImage.cs
@@ -1,6 +1,27 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Rotate Image
+/// https://leetcode.com/problems/rotate-image/
+/// </summary>
 public class P48_RotateImage
 {
-    
+    public void Rotate(int[][] matrix) {
+        // Matrix is a square matrix
+        var length = matrix.Length;
+        
+        // Transpose the matrix
+        for (var i = 0; i < length; ++i)
+        {
+            for (var j = i + 1; j < length; ++j)
+            {
+                (matrix[j][i], matrix[i][j]) = (matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // Reverse each row
+        for (var i = 0; i < length; ++i) {
+            Array.Reverse(matrix[i]);
+        }
+    }
 }
