5d2c034044e67242cacc08d8110990796a2daf43
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-22 12:29:17 +0100

    P75 to P76

--- P_Heap/P75_TopKFrequentElements.cs
+++ P_Heap/P75_TopKFrequentElements.cs
@@ -1,6 +1,37 @@
+using System.Collections.Immutable;
+
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Top K Frequent Elements
+/// https://leetcode.com/problems/top-k-frequent-elements/
+/// </summary>
 public class P75_TopKFrequentElements
 {
-    
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        // Real world implementation
+        // return nums
+        //     .GroupBy(num => num)
+        //     .OrderByDescending(num => num.Count())
+        //     .Take(k)
+        //     .Select(c => c.Key)
+        //     .ToArray();
+        
+        // To beat the quicksort implementation of OrderByDescending, we have to use the
+        // theme's problem, a heap. Here's a take using a priority queue.
+        var frequencies = nums.GroupBy(num => num);
+
+        var heap = new PriorityQueue<int, int>();
+        
+        foreach (var frequency in frequencies)
+        {
+            heap.Enqueue(frequency.Key, frequency.Count());
+            
+            if (heap.Count > k)
+                heap.Dequeue();
+        }
+        
+        return heap.UnorderedItems.Select(item => item.Element).ToArray();
+    }
 }
--- P_Heap/P76_FindMedianFromDataStream.cs
+++ P_Heap/P76_FindMedianFromDataStream.cs
@@ -1,6 +1,54 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Find Median From Data Stream
+/// https://leetcode.com/problems/find-median-from-data-stream/
+/// </summary>
 public class P76_FindMedianFromDataStream
 {
+    private readonly PriorityQueue<int, int> _maxHeap = new ();
+    private readonly PriorityQueue<int, int> _minHeap = new ();
+    private bool _isEven = true;
     
+    public P76_FindMedianFromDataStream() 
+    {
+        
+    }
+    
+    public void AddNum(int num) 
+    {
+        _isEven = !_isEven;
+        
+        if(_maxHeap.Count == 0 || _maxHeap.Peek() <= num)
+        {
+            _maxHeap.Enqueue(num, num);
+        } 
+        else 
+        {
+            _minHeap.Enqueue(num, -num);
+        }
+
+        if(_maxHeap.Count - _minHeap.Count > 1)
+        {
+            var value = _maxHeap.Dequeue();
+
+            _minHeap.Enqueue(value, -value);
+        }
+        else if(_minHeap.Count > _maxHeap.Count)
+        {
+            var value = _minHeap.Dequeue();
+
+            _maxHeap.Enqueue(value, value);
+        }
+    }
+    
+    public double FindMedian()
+    {
+        if(_minHeap.Count == 0)
+            return _maxHeap.Count == 0 ? 0.0: _maxHeap.Peek();
+        
+        return _isEven
+            ? (_minHeap.Peek() + _maxHeap.Peek()) / 2.0
+            : _maxHeap.Peek();
+    }
 }
