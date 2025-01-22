85163ecc6e853974541d95696139ff6ada62df23
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-22 14:29:37 +0100

    P20

--- P_DynamicProgramming/P20_WordBreakProblem.cs
+++ P_DynamicProgramming/P20_WordBreakProblem.cs
@@ -1,6 +1,30 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Word Break
+/// https://leetcode.com/problems/word-break/
+/// </summary>
 public class P20_WordBreakProblem
 {
-    
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        var dp = new bool[s.Length + 1];
+        dp[0] = true;
+        
+        var maxWordLength = wordDict.Max(x => x.Length);
+        
+        for(var i = 1; i <= s.Length; ++i)
+        {
+            for (var j = i - 1; j >= Math.Max(i - maxWordLength - 1, 0); --j)
+            {
+                if(dp[j] && wordDict.Contains(s[j..i]))
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[^1];
+    }
 }
