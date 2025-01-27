9fa88a5db76d159dbc4c82f1d84b6ffb96e80aba
Maxime MADER <maxime.mader@kawabytes.com>
2025-01-27 15:11:52 +0100

    P51 & P74 :p

--- P_Heap/P74_MergeKSortedLists.cs
+++ P_Heap/P74_MergeKSortedLists.cs
@@ -1,6 +1,10 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Merge K Sorted Lists
+/// https://leetcode.com/problems/merge-k-sorted-lists/
+/// </summary>
 public class P74_MergeKSortedLists
 {
-    
+    // It's the same as P43_MergeKSortedLists.cs
 }
--- P_String/P51_LongestRepeatingCharacterReplacement.cs
+++ P_String/P51_LongestRepeatingCharacterReplacement.cs
@@ -1,6 +1,45 @@
 namespace Blind75LeetCode;
 
+/// <summary>
+/// Longest Repeating Character Replacement
+/// </summary>
 public class P51_LongestRepeatingCharacterReplacement
 {
-    
+    public int CharacterReplacement(string s, int k) 
+    {
+        if(string.IsNullOrEmpty(s))
+            return 0;
+
+        var frequencies = new Dictionary<char, int>();
+        var result = 0;
+        var left = 0;
+        var right = 0;
+        var highestFrequency = 0;
+        
+        while (right < s.Length)
+        {
+            if(frequencies.ContainsKey(s[right]))
+                ++frequencies[s[right]];
+            else
+                frequencies[s[right]] = 1;
+
+            if (highestFrequency < frequencies[s[right]])
+                highestFrequency = frequencies[s[right]];
+
+            var length = right - left + 1;
+            if(length - highestFrequency > k)
+            {
+                --frequencies[s[left]];
+                ++left;
+            }
+            else
+            {
+                result = Math.Max(result, length);
+            }
+            
+            ++right;
+        }
+
+        return result;
+    }
 }
